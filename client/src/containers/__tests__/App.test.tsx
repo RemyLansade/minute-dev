3c58a164fcf0a3f14ace8b54545e3e51934706a2
RemyLansade <boblol77@live.fr>
2021-05-13 12:01:52 +0200

    Create Header component & container folder

--- client/src/containers/__tests__/App.test.tsx
+++ client/src/containers/__tests__/App.test.tsx
@@ -1,6 +1,7 @@
 import { render } from '@testing-library/react';
 import App from '../App';
 
-test('render App component', () => {
-  render(<App />)
+
+test('render App component', async () => {
+  await render(<App />);
 });
